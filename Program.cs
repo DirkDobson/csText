b997ca7bdd3829fab227260cbf440c520f97fd86
Dirk Dobson <dirkvancouver@live.com>
2018-12-02 19:19:02 -0700

    text summerized

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace csText
 {
@@ -6,7 +7,28 @@ namespace csText
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var sentence = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation";
+            const int maxLength = 20;
+            if (sentence.Length < maxLength)
+                System.Console.WriteLine(sentence);
+            else
+            {
+                var words = sentence.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach(var word in words)
+                {
+                    summaryWords.Add(word);
+
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                        break;
+                }
+
+                var summary = String.Join(" ", summaryWords) + "...";
+                System.Console.WriteLine(summary);
+            }
         }
     }
 }
